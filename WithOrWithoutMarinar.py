4094157afec8ce7e7a6b4f18dc37b5b5e391877a
Krohling <cylekrohling10@gmail.com>
2026-01-14 12:21:18 -0500

    While Loop ... 95% Stable, Doing research

--- WithOrWithoutMarinar.py
+++ WithOrWithoutMarinar.py
@@ -1,6 +1,23 @@
 #Welcome branch
 
+#Libraries Imported Here
+import sys
+import time
 
 print("\nWelcome Branch - Developer: Cyle Krohling\n")
-print("Welcome to InfoTech Center V.1.0\n")
-print("Info Tech Center is Booting Up...\n")
+print("Welcome to InfoTechCenter V.1.0\n")
+
+x = 0
+ellipsis = 0
+
+while x != 16:
+    x += 1
+    ellipsisMessage = ("InfoTechCenter OS is Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + ellipsisMessage + "   ")
+    sys.stdout.flush()
+    time.sleep(0.75)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 16:
+        print("\n\nOperating System Booted up - ID Scanned & Verified - Access Granted")
